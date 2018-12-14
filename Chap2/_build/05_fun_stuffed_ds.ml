a81ad0e1b1502bed785486360ac8d240b0858f1e
Fang Han <hanfangviolin@gmail.com>
2018-12-14 17:07:18 -0500

    day 1

--- Chap2/_build/05_fun_stuffed_ds.ml
+++
@@ -1,9 +0,0 @@
-(** anonymous functions can be stuffed in data structures *)
-
-(** a list of anonymous functions *)
-let incr_list = [(fun x -> x + 1); (fun x -> x + 2)]
-
-let new_list = List.map (fun f -> f 5) incr_list
-
-let () = 
-    print_endline(String.concat " " (List.map string_of_int new_list));
