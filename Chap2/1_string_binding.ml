5287bb60bec89445fce366aab53ea96e68079d03
Fang Han <hanfangviolin@gmail.com>
2018-12-14 15:53:25 -0500

    day 1

---
+++ Chap2/1_string_binding.ml
@@ -0,0 +1,14 @@
+(** invocation:  
+    $ ocamlbuild 1_string_binding.byte 
+    $ ./1_string_binding.byte  *)
+
+(** function signature: 
+    val concat : string -> string list -> string 
+    val split_on_char : char -> string -> string list 
+     *)
+let languages = "Ocaml, Scala, Java, C, Python"
+
+let dashed_languages l = 
+    String.concat "-" (String.split_on_char ',' l)
+
+let () = print_string (dashed_languages languages);
